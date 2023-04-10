135fed834209ee6bd03f5d2e2d48a5da47f875c2
EidorianP <adviladrien@gmail.com>
2023-04-10 12:48:33 +0200

    connexion

--- apptest.js
+++ apptest.js
@@ -2,19 +2,29 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const path = require("path");
+const mysql = require('mysql');
+const session = require('express-session');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'randomwiki'
+});
+
 
 app.set('view engine', 'pug')
 app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.static('css'));
 app.use(express.static('js'));
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
 
-/*app.use((req, res, next) => {
-    if (app.locals.extract !== '') {
-        next();
-    }
-    app.locals.extract = '';
-    next();
-});*/
 
 app.get('/test', async (req, res) => {
     const getRandomPageTitle = async () => {
@@ -26,7 +36,7 @@ app.get('/test', async (req, res) => {
             srnamespace: 0,
             rnlimit: 1,
         };
-        const response = await axios.get('https://fr.wikipedia.org/w/api.php', {params: params});
+        const response = await axios.get('https://fr.wikipedia.org/w/api.php', { params: params });
 
         return response.data.query.random[0].title;
     };
@@ -41,7 +51,7 @@ app.get('/test', async (req, res) => {
             exsectionformat: 'plain',
             exlimit: 'max',
         };
-        const response = await axios.get('https://fr.wikipedia.org/w/api.php', {params: params});
+        const response = await axios.get('https://fr.wikipedia.org/w/api.php', { params: params });
         const pageid = Object.keys(response.data.query.pages)[0];
 
         return response.data.query.pages[pageid].extract;
@@ -50,7 +60,7 @@ app.get('/test', async (req, res) => {
     const renderPage = async (title, extract) => {
         const regex = /[\p{L}\d]+/gu;
         extract = extract.replace(regex, (match) => `<span class="gamewordfalse" style="--match-length: ${match.length};">${match.length}</span>`);
-        res.render('gamepagepug', {title: title, extract: extract});
+        res.render('gamepagepug', { title: title, extract: extract });
     };
 
     const retryIfConditionsNotMet = async () => {
@@ -67,7 +77,7 @@ app.get('/test', async (req, res) => {
 
     await retryIfConditionsNotMet().catch((error) => {
         console.log(error);
-        res.status(500).json({message: 'Erreur lors de la requête'});
+        res.status(500).json({ message: 'Erreur lors de la requête' });
     });
 });
 
@@ -84,6 +94,37 @@ app.get('/search', async (req, res) => {
     }
 
     res.send('okay');
+});
+
+//
+app.all('/login', async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    // Ensure the input fields exists and are not empty
+    if (email && password) {
+
+        connection.query('SELECT * FROM accounts WHERE email = ? AND password = ?', [email, password], function (error, results, fields) {
+            if (error) throw error;
+
+            if (results.length > 0) {
+                console.log('bravo')
+                req.session.loggedin = true;
+                req.session.email = email;
+
+                 // res.redirect('/home');
+            } else {
+                console.log('Bonjour')
+                // res.send('Incorrect Username and/or Password!');
+            }
+            res.end();
+        });
+        console.log(email, password)
+    }
+    /*
+       } else {
+           res.render('login')
+       } */
+    res.render('login')
 });
 
 app.listen(3000);
