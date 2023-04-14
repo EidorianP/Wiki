5ec98d9d08a55c793c96869b8ff2d80880f201fd
EidorianP <adviladrien@gmail.com>
2023-04-14 17:08:51 +0200

    20230414

--- apptest.js
+++ apptest.js
@@ -1,177 +1,226 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
-const path = require('path');
-const mysql = require('mysql');
-const session = require('express-session');
+const path = require("path");
+const mysql = require("mysql");
+const session = require("express-session");
 const cookieParser = require("cookie-parser");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'randomwiki',
-})
-
-app.set('view engine', 'pug')
-app.set("views", path.join(__dirname, "views"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'static')));
-app.use(express.static('css'));
-app.use(express.static('js'));
-app.use(cookieParser());
-const oneDay = 1000 * 60 * 60 * 24;
-app.use(session({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized:true,
-    cookie: { maxAge: oneDay },
-    resave: false
-}));
-
-app.get('/', async (req, res) => {
-    const getRandomPageTitle = async () => {
-        const params = {
-            action: 'query',
-            format: 'json',
-            list: 'random',
-            rnlimit: 1,
-            rnnamespace: 0,
-            srnamespace: 0,
-        };
-        const response = await axios.get('https://fr.wikipedia.org/w/api.php', {params: params});
-
-        return response.data.query.random[0].title;
-    };
-
-    const getExtract = async (title) => {
-        const params = {
-            action: 'query',
-            format: 'json',
-            prop: 'extracts',
-            titles: title,
-            explaintext: false,
-            exsectionformat: 'plain',
-            exlimit: 'max',
-        };
-        const response = await axios.get('https://fr.wikipedia.org/w/api.php', {params: params});
-        const pageid = Object.keys(response.data.query.pages)[0];
-
-        return response.data.query.pages[pageid].extract;
-    };
-
-
-    const renderPage = async (title, extract) => {
-        const regex = /[\p{L}\d]+/gu;
-        extract = extract.replace(regex, (match) =>`<span class="gamewordfalse" style="--match-length: ${match.length};">${match.length}</span>`);
-
-        res.render('gamepagepug', {title: title, extract: extract, email: req.session.email, username: req.session.username});
-    };
-
-    const retryIfConditionsNotMet = async () => {
-        const title = await getRandomPageTitle();
-        app.locals.extract = await getExtract(title);
-
-        if (app.locals.extract.length < 600 || app.locals.extract.length > 11100) {
-            console.log(`Extract length: ${app.locals.extract.length} Retrying...`);
-            await retryIfConditionsNotMet();
-        } else {
-            await renderPage(title, app.locals.extract);
-        }
-    };
-
-    await retryIfConditionsNotMet().catch((error) => {
-        console.log(error);
-        res.status(500).json({message: 'Erreur lors de la requête'});
-    });
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "randomwiki",
 });
 
-app.get('/search', async (req, res) => {
-    
-    const searchQuery = req.query.query;
-    const regex = new RegExp(searchQuery, "gi");
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static("css"));
+app.use(express.static("js"));
+app.use(cookieParser());
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(
+  session({
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
 
-    // console.log(app.locals);
+app.get("/", async (req, res) => {
+  const getRandomPageTitle = async () => {
+    const params = {
+      action: "query",
+      format: "json",
+      list: "random",
+      rnlimit: 1,
+      rnnamespace: 0,
+      srnamespace: 0,
+    };
+    const response = await axios.get("https://fr.wikipedia.org/w/api.php", {
+      params: params,
+    });
 
-    if (regex.test(app.locals.extract)) {
-        const text = app.locals.extract;
-        let position = text.indexOf(searchQuery);
-        console.log(position);
-        //console.log(`Le mot "${searchQuery}" a été trouvé dans le texte.`);
-        const replacedExtract = replaceWordsInExtract(app.locals.extract, searchQuery);
-        //console.log(replacedExtract);
-        return{
-            position:position, word:searchQuery
-        }
-        //res.render('gamepagepug', {title: "Game Page", extract: replacedExtract, email: req.session.email, username: req.session.username});
+    return response.data.query.random[0].title;
+  };
+
+  const getExtract = async (title) => {
+    const params = {
+      action: "query",
+      format: "json",
+      prop: "extracts",
+      titles: title,
+      explaintext: false,
+      exsectionformat: "plain",
+      exlimit: "max",
+    };
+    const response = await axios.get("https://fr.wikipedia.org/w/api.php", {
+      params: params,
+    });
+    const pageid = Object.keys(response.data.query.pages)[0];
+
+    return response.data.query.pages[pageid].extract;
+  };
+
+  const renderPage = async (title, extract) => {
+    const regex = /[\p{L}\d]+/gu;
+    extract = extract.replace(
+      regex,
+      (match, i) =>
+        `<span class="gamewordfalse" style="--match-length: ${match.length};" id=${i}>${match.length}</span>`
+    );
+
+    res.render("gamepagepug", {
+      title: title,
+      extract: extract,
+      email: req.session.email,
+      username: req.session.username,
+    });
+  };
+
+  const retryIfConditionsNotMet = async () => {
+    const title = await getRandomPageTitle();
+    app.locals.extract = await getExtract(title);
+
+    if (app.locals.extract.length < 600 || app.locals.extract.length > 11100) {
+      console.log(`Extract length: ${app.locals.extract.length} Retrying...`);
+      await retryIfConditionsNotMet();
     } else {
-        //console.log(`Le mot "${searchQuery}" n'a pas été trouvé dans le texte.`);
-        res.send('okay');
+      await renderPage(title, app.locals.extract);
     }
+  };
+
+  await retryIfConditionsNotMet().catch((error) => {
+    console.log(error);
+    res.status(500).json({ message: "Erreur lors de la requête" });
+  });
+});
+
+app.post("/search", async (req, res) => {
+  const searchQuery = req.query.query;
+
+  const regex = new RegExp(`\\b${searchQuery}\\b`);
+
+  // const regex = new RegExp(searchQuery, "gi");
+  // console.log(app.locals);
+
+  if (regex.test(app.locals.extract)) {
+    const text = app.locals.extract;
+    const words = text.trim().split(/[\s,']+/);
+    console.log(words);
+    let positions = [];
+    words.forEach((word, i) => {
+      if (word === searchQuery) {
+        positions.push(i);
+      }
+    });
+    console.log(positions);
+    console.log(searchQuery);
+
+    // let position = text.indexOf(searchQuery);
+    // console.log(position);
+    //console.log(`Le mot "${searchQuery}" a été trouvé dans le texte.`);
+    const replacedExtract = replaceWordsInExtract(
+      app.locals.extract,
+      searchQuery
+    );
+
+    console.log({ position: positions, word: searchQuery });
+    res
+      .send({
+        status: true,
+        positions: positions,
+        word: searchQuery,
+      })
+      .status(200);
+    // res.render("gamepagepug", {
+    //   title: "Game Page",
+    //   extract: replacedExtract,
+    //   email: req.session.email,
+    //   username: req.session.username,
+    // });
+  } else {
+    //console.log(`Le mot "${searchQuery}" n'a pas été trouvé dans le texte.`);
+    res
+      .send({
+        status: false,
+      })
+      .status(200);
+  }
 });
 
 function replaceWordsInExtract(extract, searchQuery) {
-    const regex = new RegExp(searchQuery, "gi");
-    return extract.replace(regex, (match) =>
-      `<span class="gamewordtrue" style="--match-length: ${match.length};">${searchQuery}</span>`
-    ).replace(/<span class="gamewordfalse" style="--match-length: (\d+);">(\d+)<\/span>/g, (match, length) =>
-      `<span class="gamewordfalse" style="--match-length: ${length};">${searchQuery.length}</span>`
+  const regex = new RegExp(searchQuery, "gi");
+  return extract
+    .replace(
+      regex,
+      (match) =>
+        `<span class="gamewordtrue" style="--match-length: ${match.length};">${searchQuery}</span>`
+    )
+    .replace(
+      /<span class="gamewordfalse" style="--match-length: (\d+);">(\d+)<\/span>/g,
+      (match, length) =>
+        `<span class="gamewordfalse" style="--match-length: ${length};">${searchQuery.length}</span>`
     );
-  }
-
+}
 
 //
-app.get('/login', async (req, res) => {
-    res.render('login');
+app.get("/login", async (req, res) => {
+  res.render("login");
 });
 
-app.get('/register', async (req, res) => {
-    res.render('register');
+app.get("/register", async (req, res) => {
+  res.render("register");
 });
 
-app.get('/register/add', async (req, res) => {
-    // Faire le save du user ici
-    // voir https://blog.logrocket.com/building-simple-login-form-node-js/
-    // Partie insert into notamment
+app.get("/register/add", async (req, res) => {
+  // Faire le save du user ici
+  // voir https://blog.logrocket.com/building-simple-login-form-node-js/
+  // Partie insert into notamment
 });
 
-app.post('/login/check', (req, res) => {
-    let session = req.session;
-    let email = req.body.email;
-    let password = req.body.password;
+app.post("/login/check", (req, res) => {
+  let session = req.session;
+  let email = req.body.email;
+  let password = req.body.password;
 
-    if (email && password) {
-        connection.connect((error) => {
-            if (error) {
-                console.log(error)
-            } else {
-                console.log("MySQL connected!")
+  if (email && password) {
+    connection.connect((error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("MySQL connected!");
+      }
+    });
 
-            }
-        })
+    connection.query(
+      "SELECT * FROM accounts WHERE email = ? AND password = ?",
+      [email, password],
+      function (error, results, fields) {
+        if (error) throw error;
 
-        connection.query('SELECT * FROM accounts WHERE email = ? AND password = ?', [email, password], function (error, results, fields) {
-            if (error) throw error;
-        
-            if (results.length > 0) {
+        if (results.length > 0) {
+          session.loggedin = true;
+          session.email = email;
+          session.username = results[0].username;
+          console.log(req.session);
 
-                session.loggedin = true;
-                session.email = email;
-                session.username = results[0].username;
-                console.log(req.session);
+          res.redirect("/");
+        } else {
+          console.log("Incorrect Username and/or Password!");
+        }
 
-        
-                res.redirect('/');
-            } else {
-                console.log('Incorrect Username and/or Password!');
-
-            }
-        
-            res.end();
-        }, () => {
-            console.log("Deuxième requête ok");
-        });
-    }
+        res.end();
+      },
+      () => {
+        console.log("Deuxième requête ok");
+      }
+    );
+  }
 });
 
 app.listen(3000);
--- js/game.js
+++ js/game.js
@@ -1,9 +1,28 @@
-const button = document.querySelector('.gamesearchButton');
-button.addEventListener('click', function () {
-    const inputSearch = document.querySelector('.gamesearchTerm');
-    const valueSearch = inputSearch.value;
-
-    fetch(`/search?query=${valueSearch}`)
-        .then(response => console.log(response))
-        .catch(err => console.log(err));
+const button = document.querySelector(".gamesearchButton");
+button.addEventListener("click", async () => {
+  const inputSearch = document.querySelector(".gamesearchTerm");
+  const valueSearch = inputSearch.value;
+  try {
+    const response = await fetch(`/search?query=${valueSearch}`, {
+      method: "POST",
+    });
+    const data = await response.json();
+    console.log(data);
+    const word = data.word;
+    if (data.status === true) {
+      for (let i = 0; i < data.positions.length; i++) {
+        document.querySelectorAll("h5 > .gamewordfalse")[
+          data.positions[i]
+        ].textContent = word;
+        document
+          .querySelectorAll("h5 > .gamewordfalse")
+          [data.positions[i]].classList.add("gamewordtrue");
+        document
+          .querySelectorAll("h5 > .gamewordfalse")
+          [data.positions[i]].classList.remove("gamewordfalse");
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
